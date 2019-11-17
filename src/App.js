49036a3aabd30c81abd9f4fed2301691250d5d5a
Lowelys <8535760@gmail.com>
2019-11-17 22:57:17 +0300

    Add 27

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Profile from "./componets/Profile/Profile";
 import Dialogs from "./componets/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
 
+let someComponent = () => <Dialogs/>
+
 
 const App = (props) => {
     return (
@@ -14,9 +16,14 @@ const App = (props) => {
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/profile' component={Profile}/>
-                   {/* <Route path='/news' component={News}/>
+                    {/*    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>*/}
+
+                    <Route path='/dialogs' render={() => someComponent}/>
+                    <Route path='/profile' render={() => <Profile/>}/>
+
+
+                    {/* <Route path='/news' component={News}/>
                     <Route path='/music' component={Music}/>
                     <Route path='/settings' component={Settings}/>*/}
                 </div>
