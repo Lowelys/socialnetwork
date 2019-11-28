793f331195c37b1068aeec6336995dad6c04edb6
Lowelys <8535760@gmail.com>
2019-11-28 22:14:31 +0300

    Add 32-1

--- src/render.js
+++ src/render.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
 import {addPost} from './redux/state';
 import {BrowserRouter} from "react-router-dom";
 
-let rerenderEntireTree = () => {
+export let rerenderEntireTree = (state) => {
     ReactDOM.render (
         <BrowserRouter>
             <App state={state}  addPost={addPost}/>
@@ -15,8 +13,4 @@ let rerenderEntireTree = () => {
 
 }
 
-rerenderEntireTree();
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
