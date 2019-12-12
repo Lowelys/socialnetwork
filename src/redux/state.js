35eb2a970aa320c5e732a896cc989110e864bc5e
Lowelys <8535760@gmail.com>
2019-12-12 22:54:12 +0300

    Add 38-1

--- src/redux/state.js
+++ src/redux/state.js
@@ -2,68 +2,65 @@ let store = {
     _state: {
         profilePage: {
             posts: [
-                {id: 1, message: 'Hi, how are you?', likeCount: 12},
-                {id: 2, message: 'My first post', likeCount: 23},
-                {id: 3, message: 'My first post', likeCount: 23},
-                {id: 4, message: 'My first post', likeCount: 23},
-                {id: 5, message: 'My first post', likeCount: 23},
-                {id: 6, message: 'My first post', likeCount: 23},
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'It\'s my first post', likesCount: 11},
+                {id: 3, message: 'Blabla', likesCount: 11},
+                {id: 4, message: 'Dada', likesCount: 11}
             ],
             newPostText: 'it'
-
         },
         dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrew'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Viktor'},
+                {id: 6, name: 'Valera'}
+            ],
             messages: [
                 {id: 1, message: 'Hi'},
-                {id: 2, message: 'How is your'},
+                {id: 2, message: 'How is your it?'},
                 {id: 3, message: 'Yo'},
                 {id: 4, message: 'Yo'},
-                {id: 5, message: 'Yo'},
-                {id: 6, message: 'Bue'},
-            ],
-            dialogs: [
-                {id: 1, name: 'Dmitry'},
-                {id: 2, name: 'Andrey'},
-                {id: 3, name: 'Sasha'},
-                {id: 4, name: 'Sveta'},
-                {id: 5, name: 'Valera'},
-                {id: 6, name: 'Sergey'},
-            ],
+                {id: 5, message: 'Yo'}
+            ]
         },
         sidebar: {}
     },
-
-    getState() {
-        return this._state;
-    },
     _callSubscriber() {
         console.log('State changed');
+    },
+
+    getState() {
+        debugger;
+        return this._state;
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     },
 
     addPost() {
         let newPost = {
             id: 5,
             message: this._state.profilePage.newPostText,
-            likeCount: 0
+            likesCount: 0
         };
-
         this._state.profilePage.posts.push(newPost);
         this._state.profilePage.newPostText = '';
         this._callSubscriber(this._state);
     },
-
     updateNewPostText(newText) {
         this._state.profilePage.newPostText = newText;
         this._callSubscriber(this._state);
     },
+    dispatch (action) {   //{ type: 'ADD-POST'}
+if (action.type === 'ADD-POST') {
 
-    subscribe(observer) {
-        this._callSubscriber(observer);
-    },
+}
+    }
 
 }
 
-
 export default store;
-
 window.store = store;
