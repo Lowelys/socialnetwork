c300c606d9af2c71d298986729b32e84191efac6
Lowelys <8535760@gmail.com>
2019-12-16 23:43:53 +0300

    Add 40

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,8 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
+const SEND_MESSAGE = 'SEND_MESSAGE';
+
 let store = {
     _state: {
         profilePage: {
@@ -26,7 +29,8 @@ let store = {
                 {id: 3, message: 'Yo'},
                 {id: 4, message: 'Yo'},
                 {id: 5, message: 'Yo'}
-            ]
+            ],
+            newMessageBody: ''
         },
         sidebar: {}
     },
@@ -43,7 +47,7 @@ let store = {
     },
 
 
-    dispatch (action) {   //{ type: 'ADD-POST'}
+    dispatch(action) {   //{ type: 'ADD-POST'}
         if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
@@ -56,24 +60,25 @@ let store = {
         } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
+        } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+            this._state.dialogsPage.newMessageBody = action.body;
+            this._callSubscriber(this._state);
+        } else if (action.type === SEND_MESSAGE) {
+            let body = this._state.dialogsPage.newMessageBody;
+            this._state.dialogsPage.newMessageBody = '';
+            this._state.dialogsPage.messages.push({id: 6, message: 'body'});
+
+            this._callSubscriber(this._state);
         }
-    }
 
-}
-
-export const addPostActionCreator = () => {
-
-    return {
-        type: ADD_POST
     }
 }
+export const addPostActionCreator = () => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
-export const updateNewPostTextActionCreator = (text) => {
+export const sendMessageCreator = () => ({type: SEND_MESSAGE})
+export const updateNewMessageBodyCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
 
-    return {
-        type: UPDATE_NEW_POST_TEXT, newText: text
-    }
-}
 
 export default store;
 window.store = store;
