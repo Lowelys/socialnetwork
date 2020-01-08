a84f0b065072a2b5d76ada13cf582ee0b3ee2419
Lowelys <8535760@gmail.com>
2020-01-08 21:55:23 +0300

    51 Add Users API

--- src/componets/Users/Users.jsx
+++ src/componets/Users/Users.jsx
@@ -1,43 +1,20 @@
 import React from 'react';
 import styles from './users.module.css';
+import * as axios from "axios";
+import userPhoto from '../../assets/images/avatar.jpg';
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    photoUrl: 'http://magazin.static.bildkontakte.de/pics/wordpress-content/singlemagazin/uploads/2013/01/M%C3%A4nner-Macken1-80x80.jpg',
-                    followed: false,
-                    fullName: 'Max',
-                    status: 'boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'http://magazin.static.bildkontakte.de/pics/wordpress-content/singlemagazin/uploads/2013/01/M%C3%A4nner-Macken1-80x80.jpg',
-                    followed: true,
-                    fullName: 'Den',
-                    status: 'boss',
-                    location: {city: 'Pinsk', country: 'Belarus'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'http://magazin.static.bildkontakte.de/pics/wordpress-content/singlemagazin/uploads/2013/01/M%C3%A4nner-Macken1-80x80.jpg',
-                    followed: false,
-                    fullName: 'Masha',
-                    status: 'boss',
-                    location: {city: 'Brest', country: 'Belarus'}
-                },
-            ],
-        );
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        });
     }
     return <div>
         {
             props.users.map(u => <div key={u.id}>
 <span>
 <div>
-    <img src={u.photoUrl} className={styles.userPhoto}/>
+    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
 </div>
     <div>
         {u.followed
@@ -51,12 +28,12 @@ let Users = (props) => {
 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                          <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                          <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>)
