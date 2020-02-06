ef3d6db4cf8f6211f58791b7736bdfcfc5cb86ba
Lowelys <8535760@gmail.com>
2020-02-06 21:55:08 +0300

    76 redux-form2

--- src/componets/Profile/MyPosts/MyPosts.jsx
+++ src/componets/Profile/MyPosts/MyPosts.jsx
@@ -1,40 +1,39 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
+import {Field, reduxForm} from "redux-form";
 
 const MyPosts = (props) => {
-
     let postsElement =
         props.posts.map(p => <Post message={p.message} likeCount={p.likeCount}/>)
     let newPostElement = React.createRef();
-    let onAddPost = () => {
-        props.addPost();
-    }
 
-    let onPostChange = () => {
-        let text = newPostElement.current.value;
-        props.updateNewPostText(text);
-    }
+    let onAddPost = (values) => {
+        props.addPost(values.newPostText);
+    };
 
+let AddNewPostForm = (props) => {
+      return   <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field name='newPostText' component={'textarea'}/>
+            </div>
+            <div>
+                <button>Add post</button>
+            </div>
+        </form>
+    };
+
+    AddNewPostForm = reduxForm({form: 'ProfileAddNewPostForm'})(AddNewPostForm);
     return (
         <div className={s.postsBlock}>
             <h3>My post</h3>
-            <div>
-                <div>
-                    <textarea onChange={onPostChange}
-                              ref={newPostElement}
-                              value={props.newPostText}/>
-                </div>
-                <div>
-                    <button onClick={onAddPost}>Add post</button>
-                    <button>Remove</button>
-                </div>
-            </div>
+            <AddNewPostForm onSubmit={onAddPost}/>
             <div className={s.posts}>
                 {postsElement}
             </div>
         </div>
     )
-}
+};
+
 
 export default MyPosts;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,5 @@
 import {profileAPI, usersAPI} from "../api/api";
-
 const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS ';
 
@@ -12,7 +10,6 @@ let initialState = {
         {id: 3, message: 'Blabla', likesCount: 11},
         {id: 4, message: 'Dada', likesCount: 11}
     ],
-    newPostText: 'it',
     profile: null,
     status: ''
 };
@@ -22,7 +19,7 @@ const profileReducer = (state = initialState, action) => {
         case ADD_POST: {
             let newPost = {
                 id: 5,
-                message: state.newPostText,
+                message: action.newPostText,
                 likesCount: 0
             };
             return {
@@ -31,12 +28,7 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: ''
             };
         }
-        case UPDATE_NEW_POST_TEXT: {
-            return {
-                ...state,
-                newPostText: action.newText
-            }
-        }
+
         case SET_STATUS: {
             return {
                 ...state,
@@ -51,7 +43,7 @@ const profileReducer = (state = initialState, action) => {
     }
 };
 
-export const addPostActionCreator = () => ({type: ADD_POST});
+export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText});
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 export const setStatus = (status) => ({type: SET_STATUS, status});
 
@@ -74,6 +66,5 @@ export const updateStatus = (status) => (dispatch) => {
             }
         });
 };
-export const updateNewPostTextActionCreator = (text) =>
-    ({type: UPDATE_NEW_POST_TEXT, newText: text});
+
 export default profileReducer;
