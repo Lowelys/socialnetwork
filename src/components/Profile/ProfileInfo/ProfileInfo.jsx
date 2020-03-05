9264c8e259f7687cfd7eadbe90b6ecb5d54df54d
Sergey Belov <48603698+Lowelys@users.noreply.github.com>
2020-03-05 17:50:57 +0300

    96 houldComponentUpdate fix

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
-
-const ProfileInfo = ({profile, status, updateStatus}) => {
+import userPhoto from "../../../assets/images/user.png";
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto }) => {
     if (!profile) {
         return <Preloader/>
     }
+    const onMainPhotoSelected = (e) => {
+        if (e.target.files.length) {
+            savePhoto(e.target.files[0]);
+        }
+    };
 
     return (
         <div>
             <div>
-                <img src='https://c.radikal.ru/c07/1911/02/b3aec4a81cbe.jpg'/>
+                <img src='https://c.radikal.ru/c07/1911/02/b3aec4a81cbe.jpg' className={s.imgBlock}/>
             </div>
             <div className={s.descriptionBlock}>
-                <img src={profile.photos.large}/>
+                <img src={profile.photos.large || userPhoto} className={s.mainPhoto}/>
+                {isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
 
             </div>
