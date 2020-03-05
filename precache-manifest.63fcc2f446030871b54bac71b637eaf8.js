83fb474f00899daae582548aa89eebda71b7ee02
Sergey Belov <48603698+Lowelys@users.noreply.github.com>
2020-03-05 12:29:25 +0300

    Updates

---
+++ precache-manifest.63fcc2f446030871b54bac71b637eaf8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e36653436a86fc1356ecb57b0f95023",
+    "url": "/socialnetwork/index.html"
+  },
+  {
+    "revision": "9f5f7b10f73fe559e23b",
+    "url": "/socialnetwork/static/css/3.6caec354.chunk.css"
+  },
+  {
+    "revision": "6f307435d2c66a1a2510",
+    "url": "/socialnetwork/static/css/4.6a8f3c1f.chunk.css"
+  },
+  {
+    "revision": "ecf7a969751bc371d4c1",
+    "url": "/socialnetwork/static/css/main.3b0300d3.chunk.css"
+  },
+  {
+    "revision": "3e5c118a8aac20554b55",
+    "url": "/socialnetwork/static/js/2.ec8f3ee3.chunk.js"
+  },
+  {
+    "revision": "9f5f7b10f73fe559e23b",
+    "url": "/socialnetwork/static/js/3.c7fe9e57.chunk.js"
+  },
+  {
+    "revision": "6f307435d2c66a1a2510",
+    "url": "/socialnetwork/static/js/4.f8ab13e9.chunk.js"
+  },
+  {
+    "revision": "ecf7a969751bc371d4c1",
+    "url": "/socialnetwork/static/js/main.b003fc3d.chunk.js"
+  },
+  {
+    "revision": "4851acf8bd35134f52b7",
+    "url": "/socialnetwork/static/js/runtime-main.86773bf6.js"
+  },
+  {
+    "revision": "1dd4b4ecc8a4bdfb53b78b24c2266283",
+    "url": "/socialnetwork/static/media/Spinner.1dd4b4ec.svg"
+  },
+  {
+    "revision": "2911e64c8108fe03f595344400db9e79",
+    "url": "/socialnetwork/static/media/avatar.2911e64c.jpg"
+  }
+]);
